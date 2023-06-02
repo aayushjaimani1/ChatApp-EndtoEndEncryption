7e0f3b8e620d69e58bc71cf5f55962e706b2ea51
Aayush Jaimani <aayush.2442@gmail.com>
2023-06-02 13:44:33 +0530

    Main Client

--- NewClient.java
+++ NewClient.java
@@ -66,3 +66,93 @@ public class NewClient extends JFrame implements ActionListener {
         send.setFont(new Font("SAN_SERIF", Font.PLAIN, 16));
         add(send);
     }
+
+    public void actionPerformed(ActionEvent e) {
+        try {
+            String str = text.getText();
+            JPanel p2 = formatLabel(str);
+
+            mp.setLayout(new BorderLayout());
+
+            JPanel right = new JPanel(new BorderLayout());
+            right.add(p2, BorderLayout.LINE_END);
+            vertical.add(right);
+            vertical.add(Box.createVerticalStrut(15));
+
+            mp.add(vertical, BorderLayout.PAGE_START);
+
+            String encodedMessage = encodeMessage(str);
+            System.out.println("Encoded Message: " + encodedMessage);
+            dout.writeUTF(encodedMessage);
+
+            text.setText("");
+
+            repaint();
+            invalidate();
+            validate();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static JPanel formatLabel(String str) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel output = new JLabel("<html><p style=\"width: 150px\">" + str + "</p></html>");
+        output.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        output.setBackground(new Color(4, 118, 208));
+        output.setOpaque(true);
+        output.setBorder(new EmptyBorder(15, 15, 15, 50));
+
+        panel.add(output);
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+        JLabel time = new JLabel();
+        time.setText(sdf.format(cal.getTime()));
+
+        panel.add(time);
+
+        return panel;
+    }
+
+    public static String encodeMessage(String message) {
+        byte[] encodedBytes = Base64.getEncoder().encode(message.getBytes());
+        return new String(encodedBytes);
+    }
+
+    public static String decodeMessage(String encodedMessage) {
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedMessage.getBytes());
+        return new String(decodedBytes);
+    }
+
+    public static void main(String[] args) {
+        NewClient client = new NewClient();
+
+        try {
+            Socket s = new Socket("127.0.0.1", 6001);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            dout = new DataOutputStream(s.getOutputStream());
+
+            while (true) {
+                mp.setLayout(new BorderLayout());
+                String encodedMsg = din.readUTF();
+                String decodedMsg = decodeMessage(encodedMsg);
+                JPanel panel = formatLabel(decodedMsg);
+
+                JPanel left = new JPanel(new BorderLayout());
+                left.add(panel, BorderLayout.LINE_START);
+                vertical.add(left);
+
+                vertical.add(Box.createVerticalStrut(15));
+                mp.add(vertical, BorderLayout.PAGE_START);
+
+                client.validate();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
